3a91896d6c926d11886ee83f174d86042fd9da28
snyh <snyh@snyh.org>
2015-04-21 00:34:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+[Gopher China 2015 Deepin](http://go-talks.appspot.com/github.com/snyh/slide/gopherchina2015/deepin.slide)
---
+++ gopherchina2015/x.go
@@ -0,0 +1,11 @@
+package main
+
+import  strings
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("Playing..WuHan...........", strings.Join([]string{"a","b","c"}, "+"))
+}
